8896c2e7cf74f45679958f1fe422df3d64d97b44
oiuv <i@oiuv.cn>
2018-05-15 00:23:23 +0800

    Finish layout and routes

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,13 @@
+<header class="navbar navbar-fixed-top navbar-inverse">
+    <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+            <a href="{{ route('home') }}" id="logo">Sample App</a>
+            <nav class="float-right">
+                <ul class="nav nav-pills">
+                    <li class="nav-link"><a href="{{ route('help') }}">帮助</a></li>
+                    <li class="nav-link"><a href="#">登录</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
