5f5453b776b81da82035dd8252fa504854d42376
oiuv <i@oiuv.cn>
2018-06-04 21:17:15 +0800

    npm run prod

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -127,7 +127,7 @@ class UsersController extends Controller
         //$from = 'i@chenxuefeng.cn';
         //$name = 'Xuefeng';
         $to = $user->email;
-        $subject = "感谢注册 Sample 应用！请确认你的邮箱。";
+        $subject = "感谢注册！请确认你的邮箱。";
 
         Mail::send($view, $data, function ($message) use ($to, $subject) {
             //$message->from($from, $name)->to($to)->subject($subject);
