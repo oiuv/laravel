23ede477e496ac90abacac16984274f2c45f3fdf
oiuv <i@oiuv.cn>
2018-12-24 15:01:01 +0800

    update to laravel 5.7

--- database/factories/StatusFactory.php
+++ database/factories/StatusFactory.php
@@ -3,10 +3,15 @@
 use Faker\Generator as Faker;
 
 $factory->define(\App\Models\Status::class, function (Faker $faker) {
-    $date_time = $faker->date . ' ' . $faker->time;
+
+    // 随机取一个月以内的时间
+    $updated_at = $faker->dateTimeThisMonth();
+    // 传参为生成最大时间不超过，创建时间永远比更改时间要早
+    $created_at = $faker->dateTimeThisMonth($updated_at);
+
     return [
-        'content'    => $faker->text(),
-        'created_at' => $date_time,
-        'updated_at' => $date_time,
+        'content'    => $faker->catchPhrase,
+        'created_at' => $created_at,
+        'updated_at' => $updated_at,
     ];
 });
